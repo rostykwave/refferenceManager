132afa6116ddd84d5a1c4778280e97b7d6ea6b85
Rostyslav <rostykwave@gmail.com>
2024-08-16 23:31:47 +0300

    feat: refference formatting v1

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const fs = require("fs").promises;
-const { parseKeyValue } = require("./src/helpers/parseKeyValue");
+function translateReference(input) {
+  const parts = input.split("/");
+  const title = parts[0].trim();
+  const authorsSet = parts[1].trim();
+  const journalInfo = parts[3].trim();
 
-async function updateReferences() {
-  try {
-    // Parse key-value pairs from matches.txt
-    const keyValue = await parseKeyValue("./src/matches-set.txt");
+  // Extracting the authors
+  const authors = authorsSet
+    .trim()
+    .split(",")
+    .map((author) => author.trim());
 
-    // Read content from beforeUpdate.txt
-    const content = await fs.readFile("./src/beforeUpdate.txt", "utf-8");
+  // Reconstructing the output in the desired format
+  const formattedAuthors = authors
+    .map((author) => {
+      const authorChain = author.trim().split(".");
+      const updAuthor = `${authorChain[2].trim()} ${authorChain[0].trim()}.${authorChain[1].trim()}.`;
+      return updAuthor;
+    })
+    .join(", ");
+  const result = `${formattedAuthors} ${title}. ${journalInfo}`;
 
-    // Replace old reference numbers with new ones
-    const updatedContent = content.replace(/\[(\d+)\]/g, (match, p1) => {
-      const newValue = keyValue[parseInt(p1, 10)];
-      return newValue ? `[${newValue}]` : match;
-    });
-
-    // Write the updated content to updated.txt
-    await fs.writeFile("./src/updated.txt", updatedContent);
-    console.log("Оновлений текст записано у файл updated.txt");
-  } catch (error) {
-    console.error("Помилка при роботі з файлами:", error);
-  }
+  return result;
 }
 
-updateReferences();
+const input =
+  "Нові можливості оцінювання ризику розвитку госпітальних ускладнень у хворих з гострим інфарктом міокарда з елевацією сегмента ST за даними вивчення клітинного складу крові / О. М. Пархоменко, О. В. Шумаков, Т. В. Талаєва, І. В. Третяк, О. В. Довгань // Український кардіологічний журнал. 2021. Т. 28, № 1. С. 7-17.";
+const output = translateReference(input);
+//Пархоменко О.М., Шумаков О.В., Талаєва Т.В., Третяк І.В., Довгань О.В. Нові можливості оцінювання ризику розвитку госпітальних ускладнень у хворих з гострим інфарктом міокарда з елевацією сегмента ST за даними вивчення клітинного складу крові. Український кардіологічний журнал. 2021. Т. 28, № 1. С. 7-17.
+
+console.log(output);
