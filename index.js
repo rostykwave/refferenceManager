6f92ba75afd8b65d9aa6281824787feba040d99b
Rostyslav <rostykwave@gmail.com>
2025-02-22 15:45:51 +0200

    feat: add ability to find multiple numbers [1,2,3]

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { parseKeyValue } = require("./src/helpers/parseKeyValue");
 
 async function updateReferences() {
   try {
+    const defaultNumberValue = 1000;
     // Parse key-value pairs from matches.txt
     const keyValue = await parseKeyValue("./src/matches-set.txt");
 
@@ -10,10 +11,17 @@ async function updateReferences() {
     const content = await fs.readFile("./src/beforeUpdate.txt", "utf-8");
 
     // Replace old reference numbers with new ones
-    const updatedContent = content.replace(/\[(\d+)\]/g, (match, p1) => {
-      const newValue = keyValue[parseInt(p1, 10)];
-      return newValue ? `[${newValue}]` : match;
-    });
+    const updatedContent = content.replace(
+      /\[(\d+(?:,\s*\d+)*)\]/g,
+      (match, numbers) => {
+        const updatedNumbers = numbers.split(",").map((num) => {
+          const newValue = keyValue[parseInt(num.trim(), 10)];
+          return newValue ? newValue : defaultNumberValue;
+        });
+
+        return `[${updatedNumbers.join(", ")}]`;
+      }
+    );
 
     // Write the updated content to updated.txt
     await fs.writeFile("./src/updated.txt", updatedContent);
